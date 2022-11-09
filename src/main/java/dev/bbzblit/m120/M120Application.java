753bb7acec5f7e05161f04de638a6f8e9e379fe8
Yannick Mueller <yannick.mueller@fusiva.ch>
2022-11-09 11:14:08 +0100

    add spring boot project

---
+++ src/main/java/dev/bbzblit/m120/M120Application.java
@@ -0,0 +1,13 @@
+package dev.bbzblit.m120;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M120Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M120Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bbzblit/m120/M120ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bbzblit.m120;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M120ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
