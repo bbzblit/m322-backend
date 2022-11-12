475f99f9d250a0ca2a881283896e4a16ca59f9b0
Yannick Mueller <yannick.mueller@fusiva.ch>
2022-11-12 16:49:56 +0100

    add basic models

---
+++ src/main/java/dev/bbzblit/m120/models/Grade.java
@@ -0,0 +1,20 @@
+package dev.bbzblit.m120.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Document
+@Data
+public class Grade {
+
+	@Id
+	private String id;
+	
+	private double weight;
+	
+	private double value;
+	
+	
+}
